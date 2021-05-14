800187c525b17c2ab042ef38c525c2f12321eb93
João Mota <joaojorgemota@gmail.com>
2021-05-14 12:21:05 +0100

    Forms Funcionarios/Pareceres

---
+++ Projecto_de_DA/Funcionários.Designer.cs
@@ -0,0 +1,48 @@
+﻿
+namespace Projecto_de_DA
+{
+    partial class Funcionários
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Funcionários
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(874, 414);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Funcionários";
+            this.Text = "Funcionários";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
